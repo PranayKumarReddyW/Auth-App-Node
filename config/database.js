3b9571fbf88d3c6ebe6c1f8e83d76c4a4cc1c804
PranayKumarReddyW <pranaykumarreddyw@gmail.com>
2024-08-16 12:35:25 +0530

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
