0ccbd50abcc3e0b8e7e1d42ddf47230f55111965
PranayKumarReddyW <118808874+PranayKumarReddyW@users.noreply.github.com>
2024-08-22 22:10:17 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  "username": {
+    type: String,
+    required: true, // Ensure email is required
+    unique: true,   // Ensure email is unique
+  },
+  "name":{
+     type: String,
+    required: true
+  },
+  "password":{
+    type: String, 
+    required: true 
+  }
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
