a806427e2237bb6893538df3547a4e6efe729dde
PranayKumarReddyW <118808874+PranayKumarReddyW@users.noreply.github.com>
2024-08-22 11:24:30 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,19 @@
+const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+require("dotenv").config();
 
-exports.connect = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+const connect = mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+
+    return error;
+  });
+
+module.exports = connect;
